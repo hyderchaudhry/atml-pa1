36367b7110bc4a28742c3271126d818ca68373b8
Hyder Chaudhry <mhyderchaudhry@gmail.com>
2026-09-23 01:55:31 +0500

    Prepare for running on colab

--- task1/models/models.py
+++
@@ -1,7 +0,0 @@
-import torch
-import open_clip
-from torchvision import datasets, models, transforms
-
-resnet_50 = models.resnet50(weights='ResNet50_Weights.IMAGENET1K_V2')
-vit_model = models.vit_b_16(weights='ViT_B_16_Weights.IMAGENET1K_V1')
-
